b38c4c9fcd05d803eeec586909d8cd171ddca7b3
Valerii Gordon <56223756+ValeriyGordon@users.noreply.github.com>
2022-02-15 21:25:07 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// swiper
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: true,
+  },
+  speed: 1000,
+  effect: 'slide',
+
+  // пагинация
+  pagination: {
+  el: '.swiper-pagination',
+  clickable: true
+
+  },
+  // навигация
+  navigation: {
+  nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev',
+  },
+
+  });
+
+  // tabs
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.how__steps-item').forEach(function(howStepsItem) {
+      howStepsItem.addEventListener('click', function(event) {
+        const path = event.currentTarget.dataset.path
+
+        document.querySelectorAll('.how__tabs').forEach(function(howTabs) {
+          howTabs.classList.remove('how__tabs-active')
+        })
+        document.querySelector(`[data-target="${path}"]`).classList.add('how__tabs-active')
+      })
+    })
+  })
